2c5cb17e35bdc7d0f4fd545e55b1e4ecbbc2dd3c
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-05-26 10:01:06 +0800

    update app_config.js

--- app_config.js
+++ app_config.js
@@ -4,8 +4,8 @@ Apk_appVersion="1.0.2";
 Apk_dateTime="2023-11-03";
 Apk_content="修复部分已知Bug，适配安卓11系统以上版本，“使用教程” 在软件右上角，里面有软件的使用方法。";
 /*Apk_content2="修复部分已知Bug，重新下载并且安装后此弹窗不在提示，本次属于强制更新，当前软件已上架至 “酷安市场” 酷安上搜索 “Post提交工具” 就能找到这个软件。";*/
-Apk_downurl="https://gitee.com/qsth520/appjs/releases/download/staticpages/Post%E6%8F%90%E4%BA%A4%E5%B7%A5%E5%85%B7_1.0.2.apk";
-Apk_downurl2="https://gitee.com/qsth520/appjs/releases/download/staticpages/Post%E6%8F%90%E4%BA%A4%E5%B7%A5%E5%85%B7_1.0.2.apk";
+Apk_downurl="https://gitee.com/qsth520/appjs/releases/download/staticpages/Post%E6%8F%90%E4%BA%A4%E5%B7%A5%E5%85%B7_v1.0.2.apk";
+Apk_downurl2="https://gitee.com/qsth520/appjs/releases/download/staticpages/Post%E6%8F%90%E4%BA%A4%E5%B7%A5%E5%85%B7_v1.0.2.apk";
 Apk_downurl3="https://note.youdao.com/yws/api/personal/file/38456afc083f8675ad346e0912e709fb?method=download&shareKey=72db0ce5f1e7c9b718a6aa6d46b95fa3&inline=true";
 }catch(e){};
 })();
